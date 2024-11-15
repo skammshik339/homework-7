57a6ffc9f926ab54e939d8c0302221474d6cda8e
skammshik339 <mozartrumpet@gmail.com>
2024-11-15 22:38:18 +0300

    add ucFirst

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ class Mystring {
     arr = arr.reverse()
     return arr.join('')
   }
+  ucFirst(string) {
+    return string[0].toUpperCase() + string.slice(1,)
+  }
 }
 let str = new Mystring();
 console.log(str.reverseStr('abcde'));
+console.log(str.ucFirst('abcde'));
