d5229675a7115fd14c334e2e354f0e6b6e88ab09
skammshik339 <mozartrumpet@gmail.com>
2024-11-15 22:41:47 +0300

    add ucWords

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ class Mystring {
   ucFirst(string) {
     return string[0].toUpperCase() + string.slice(1,)
   }
+  ucWords(string) {
+    let arr = string.split(' ')
+    arr.forEach((element, i, array) => {
+      array[i] = array[i][0].toUpperCase() + array[i].slice(1,)
+    });
+    return arr.join(' ')
+  }
 }
 let str = new Mystring();
 console.log(str.reverseStr('abcde'));
 console.log(str.ucFirst('abcde'));
+console.log(str.ucWords('abcde abcde abcde'));
